a5fc4ee4f5c25fe1035fc2957ff9dcbe985bede3
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-02-23 22:39:28 -0300

    Atualizando classe

--- src/main/java/com/herokuapp/colorebackend/Colore/repositories/CurriculoRepository.java
+++ src/main/java/com/herokuapp/colorebackend/Colore/repositories/CurriculoRepository.java
@@ -2,9 +2,10 @@ package com.herokuapp.colorebackend.Colore.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.herokuapp.colorebackend.Colore.models.Curriculo;
 
 @Repository
-public interface CurriculoRepository extends JpaRepository<Package, Integer>{
+public interface CurriculoRepository extends JpaRepository<Curriculo, Integer>{
 
 }
 
--- src/main/java/com/herokuapp/colorebackend/Colore/repositories/EnderecoRepository.java
+++ src/main/java/com/herokuapp/colorebackend/Colore/repositories/EnderecoRepository.java
@@ -1,7 +1,8 @@
 package com.herokuapp.colorebackend.Colore.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.herokuapp.colorebackend.Colore.models.Endereco;
 
-public interface EnderecoRepository extends JpaRepository<Package, Integer>{
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
 
 }
