f62ebf4a40be6f6344622a1d70bd807759d95e27
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-02-23 22:42:34 -0300

    Adicionando classe responsavel pelo acesso a dados de Formação - FormacaoRepository

---
+++ src/main/java/com/herokuapp/colorebackend/Colore/repositories/FormacaoRepository.java
@@ -0,0 +1,8 @@
+package com.herokuapp.colorebackend.Colore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.herokuapp.colorebackend.Colore.models.Formacao;
+
+public interface FormacaoRepository extends JpaRepository<Formacao, Integer>{
+
+}
