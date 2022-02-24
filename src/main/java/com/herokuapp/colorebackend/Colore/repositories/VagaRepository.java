61e97d528bada0bfe5f346e0a10f2f9d759fd4b1
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-02-23 22:50:54 -0300

    Adicionando classe responsavel pelo acesso a dados Vaga - VagaRepository

---
+++ src/main/java/com/herokuapp/colorebackend/Colore/repositories/VagaRepository.java
@@ -0,0 +1,8 @@
+package com.herokuapp.colorebackend.Colore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.herokuapp.colorebackend.Colore.models.Vaga;
+
+public interface VagaRepository extends JpaRepository<Vaga, Integer>{
+
+}
