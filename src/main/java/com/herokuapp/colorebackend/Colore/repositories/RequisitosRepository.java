54397e2f13a00c1e518cc4d7a1e3beea41c3395c
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-02-23 22:49:39 -0300

    Adicionando classe responsavel pelo acesso a dados de Requisito - RequisitosRepository

---
+++ src/main/java/com/herokuapp/colorebackend/Colore/repositories/RequisitosRepository.java
@@ -0,0 +1,8 @@
+package com.herokuapp.colorebackend.Colore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.herokuapp.colorebackend.Colore.models.Requisitos;
+
+public interface RequisitosRepository extends JpaRepository<Requisitos, Integer>{
+
+}
