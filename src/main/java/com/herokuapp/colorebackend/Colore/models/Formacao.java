1f5aedf6b1873ce749ad0e3c9c49f323753bec04
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-02-23 21:45:08 -0300

    Adicionando Entidade Formação - Models

---
+++ src/main/java/com/herokuapp/colorebackend/Colore/models/Formacao.java
@@ -0,0 +1,127 @@
+package com.herokuapp.colorebackend.Colore.models;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table
+public class Formacao {
+	@GeneratedValue
+	@Id
+	private int id;
+	@Column(nullable = false)
+	private String titulo;
+	@Column(nullable = false)
+	private String nivel;
+	@Column(nullable = false)
+	private String instituicao;
+	@Column(nullable = false)
+	private LocalDate dataInicio;
+	private LocalDate dataFim;
+	private String caminhoImagemDiploma;
+	
+	public Formacao() {
+		super();
+	}
+
+	public Formacao(int id, String titulo, String nivel, String instituicao, LocalDate dataInicio, LocalDate dataFim,
+			String caminhoImagemDiploma) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.nivel = nivel;
+		this.instituicao = instituicao;
+		this.dataInicio = dataInicio;
+		this.dataFim = dataFim;
+		this.caminhoImagemDiploma = caminhoImagemDiploma;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+	}
+
+	public String getInstituicao() {
+		return instituicao;
+	}
+
+	public void setInstituicao(String instituicao) {
+		this.instituicao = instituicao;
+	}
+
+	public LocalDate getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(LocalDate dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public LocalDate getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(LocalDate dataFim) {
+		this.dataFim = dataFim;
+	}
+
+	public String getCaminhoImagemDiploma() {
+		return caminhoImagemDiploma;
+	}
+
+	public void setCaminhoImagemDiploma(String caminhoImagemDiploma) {
+		this.caminhoImagemDiploma = caminhoImagemDiploma;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(caminhoImagemDiploma, dataFim, dataInicio, id, instituicao, nivel, titulo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Formacao other = (Formacao) obj;
+		return Objects.equals(caminhoImagemDiploma, other.caminhoImagemDiploma)
+				&& Objects.equals(dataFim, other.dataFim) && Objects.equals(dataInicio, other.dataInicio)
+				&& id == other.id && Objects.equals(instituicao, other.instituicao)
+				&& Objects.equals(nivel, other.nivel) && Objects.equals(titulo, other.titulo);
+	}
+
+	@Override
+	public String toString() {
+		return "Formacao [id=" + id + ", titulo=" + titulo + ", nivel=" + nivel + ", instituicao=" + instituicao
+				+ ", dataInicio=" + dataInicio + ", dataFim=" + dataFim + ", caminhoImagemDiploma="
+				+ caminhoImagemDiploma + "]";
+	}	
+	
+}
