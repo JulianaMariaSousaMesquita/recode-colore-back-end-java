e03a144d94db7cf9b843c0393d964c7ed8f573c0
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-02-23 22:37:39 -0300

    Adicionando classe responsavel pelo acesso a dados Endereco - EnderecoRepository

---
+++ src/main/java/com/herokuapp/colorebackend/Colore/repositories/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.herokuapp.colorebackend.Colore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Package, Integer>{
+
+}
