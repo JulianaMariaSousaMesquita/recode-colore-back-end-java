8308206c27065ffcb1ec4479d98991606144a82d
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-02-23 21:32:55 -0300

    Adicionando Entidade Endereço - Models

---
+++ src/main/java/com/herokuapp/colorebackend/Colore/models/Endereco.java
@@ -0,0 +1,142 @@
+package com.herokuapp.colorebackend.Colore.models;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table
+public class Endereco {
+	@GeneratedValue
+	@Id
+	private int id;
+	@Column(nullable = false)
+	private int cep;
+	@Column(nullable = false)
+	private String rua;
+	private int numero;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String uf;
+	private String pais;
+	
+	public Endereco() {
+		super();
+	}
+
+	public Endereco(int id, int cep, String rua, int numero, String complemento, String bairro, String cidade,
+			String uf, String pais) {
+		super();
+		this.id = id;
+		this.cep = cep;
+		this.rua = rua;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.uf = uf;
+		this.pais = pais;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCep() {
+		return cep;
+	}
+
+	public void setCep(int cep) {
+		this.cep = cep;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(bairro, cep, cidade, complemento, id, numero, pais, rua, uf);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		return Objects.equals(bairro, other.bairro) && cep == other.cep && Objects.equals(cidade, other.cidade)
+				&& Objects.equals(complemento, other.complemento) && id == other.id && numero == other.numero
+				&& Objects.equals(pais, other.pais) && Objects.equals(rua, other.rua) && Objects.equals(uf, other.uf);
+	}
+
+	@Override
+	public String toString() {
+		return "Endereco [id=" + id + ", cep=" + cep + ", rua=" + rua + ", numero=" + numero + ", complemento="
+				+ complemento + ", bairro=" + bairro + ", cidade=" + cidade + ", uf=" + uf + ", pais=" + pais + "]";
+	}	
+	
+}
