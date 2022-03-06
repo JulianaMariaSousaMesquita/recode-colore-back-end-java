d786f4da27f883341ad38bf278087997005469db
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-03-05 22:23:12 -0300

    Atualizando importação anotação id

--- src/main/java/com/herokuapp/colorebackend/Colore/models/Habilidades.java
+++ src/main/java/com/herokuapp/colorebackend/Colore/models/Habilidades.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
-import org.springframework.data.annotation.Id;
+import javax.persistence.Id;
 
 @Entity
 @Table
